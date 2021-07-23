302bd96a8be38f80b577f73d1b886b62e4e92356
Yury <yp@unotex.ru>
2021-07-23 12:14:01 +0700

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int money_order = 500_000_000;
+        long total = balance + money_order;
+        System.out.println(total);
+    }
+}
+
